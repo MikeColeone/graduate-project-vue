89f1c627cb0369b172b4a4f2072a325ba957881e
MikeColeone <coleonemike@gmail.com>
2024-10-25 22:41:17 +0800

    aside

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
 import App from "./App.vue";
+import router from "./router";
+import "element-plus/dist/index.css"; // 引入 Element Plus 的样式
 // 全局引入
 const app = createApp(App);
 app.use(ElementPlus);
+app.use(router);
 app.mount("#app");
